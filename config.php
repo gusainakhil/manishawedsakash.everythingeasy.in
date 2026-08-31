8c62ce07d234aff3354e8b250fb1a1dd45392383
Akhil gusain <akhilgusain2@gmail.com>
2026-09-01 00:55:30 +0530

    config file

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ return [
         'host' => 'localhost',
         'port' => 3306,
         'name' => 'manishawedsakash.everythingeasy.in',
-        'user' => 'manishawedsakash.everythingeasy.in',
+        'user' => 'manishawedsakash',
         'pass' => 'manishawedsakash.everythingeasy.in',
         'charset' => 'utf8mb4',
     ],
